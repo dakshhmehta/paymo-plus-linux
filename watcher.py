2cf765a5dd3e4a57b81249069ac765cf047a3f4b
Daksh Mehta <daksh@dristal.com>
2015-05-31 23:22:40 +0530

    Away Time detector

--- watcher.py
+++ watcher.py
@@ -4,8 +4,15 @@ import threading
 import csv
 import os
 
+_setting = {
+	"away_time": 300 # 5 minute
+}
+
 timer = {}
 file_name = "timesheet_%s.csv"%time.time()
+
+away_time = 0;
+last_task = "";
 
 def write_to_file():
 	global timer, file_name
@@ -27,15 +34,31 @@ def print_to_screen():
    	return
 
 def record_windows(winTitle):
-	global timer
+	global timer, last_task, away_time, _setting
+
+	_t = 1
+
+	# If task is same as previous
+	if last_task == winTitle:
+		away_time = away_time + 1 # Possiblity of away
+
+		if away_time == _setting['away_time']: # If its away for 1 minute
+			timer[winTitle] = timer[winTitle] - _setting['away_time']
+			winTitle = "Away Time"
+			_t = _setting['away_time']
+			away_time = 0
+	else:
+		away_time = 0
 
 	timer.setdefault(winTitle, 0)
-	timer[winTitle] = timer[winTitle] + 1
+	timer[winTitle] = timer[winTitle] + _t
 
 	t = threading.Thread(name='FileWriter', target=write_to_file)
 	t.start()
 
 	print_to_screen()
+
+	last_task = winTitle
 
 	return
 
